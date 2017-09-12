f421d935ef303ff519f3a6eedd2ec9443d82443e
Ubuntu <ubuntu@terraform.compute-a425187.oraclecloud.internal>
2017-09-12 09:56:36 +0000

    update swarm token

--- worker.sh
+++ worker.sh
@@ -1,6 +1,6 @@
 curl -sSL https://get.docker.com | sudo bash
 
-sudo docker swarm join --token=<Tokenkey> --listen-addr eth0 --advertise-addr eth0  <Private master ip>
+sudo docker swarm join --token=SWMTKN-1-2x8fmtf1aticjjv4wd7y21ebzqbagq12k4yo7lxkp4y538lyo0-bcxugirs4phrvrk8rd0qzape0 --listen-addr eth0 --advertise-addr eth0  10.196.45.166:2377
 
 sleep 30
 sudo init 6
