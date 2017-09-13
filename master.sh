aa14e4b87980c84805b5a94b14a909ce83d077be
Ubuntu <ubuntu@terraform.compute-a425187.oraclecloud.internal>
2017-09-13 07:21:10 +0000

    added delay time

--- master.sh
+++ master.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+sleep 5
 curl -sSL https://get.docker.com | sudo bash
+sleep 2
 sudo docker swarm init --listen-addr eth0  --advertise-addr eth0
 sleep 30
 sudo init 6
--- worker.sh
+++ worker.sh
@@ -1,4 +1,7 @@
+#/bin/bash
+sleep 5
 curl -sSL https://get.docker.com | sudo bash
+sleep 2
 
 sudo docker swarm join --token=SWMTKN-1-4e0dgsdtq987a82au4kdxbgegwhwbtso5s0cxfip8ad6tx8f5p-8ewlxtjm1l44bdvahr5mgmhhw --listen-addr eth0 --advertise-addr eth0  10.196.37.42:2377
 
